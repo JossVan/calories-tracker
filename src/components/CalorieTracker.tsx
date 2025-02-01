a62517f4e8469a2248e1864d2cb52217b730e7c9
jossvan <vanessapolanco.tech@gmail.com>
2025-01-31 23:49:15 -0600

    diference added

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -25,6 +25,11 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
       ),
     [activities]
   );
+
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [activities]
+  );
   return (
     <>
       <h2 className="text-4xl font-bold text-white text-center">
@@ -33,6 +38,7 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
         <CalorieDisplay calories={caloriesConsumed} text={"Consumidas"} />
         <CalorieDisplay calories={caloriesBurned} text={"Quemadas"} />
+        <CalorieDisplay calories={netCalories} text={"Diferencia"} />
       </div>
     </>
   );
